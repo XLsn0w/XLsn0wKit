75d5108d3f0a4b516e5c075cdc4cdf9f64adc534
XLsn0w <xlsn0w@qq.com>
2016-09-11 16:02:12 +0800

    v3.4.1

--- XLsn0w/XLsn0wKit.podspec
+++ XLsn0w/XLsn0wKit.podspec
@@ -1,7 +1,7 @@
 
 Pod::Spec.new do |s|
 
-  s.version      = "3.4.0"
+  s.version      = "3.4.1"
 
   s.name         = "XLsn0wKit"
   s.summary      = 'A collection of iOS components from XLsn0wKit'
@@ -15,7 +15,6 @@ Pod::Spec.new do |s|
   s.source       = { :git => "https://github.com/XLsn0w/XLsn0wKit.git", :tag => s.version.to_s }
 
   s.source_files = "XLsn0w/**/*.{h,m}"
-  s.public_header_files = 'XLsn0w/**/*.{h}'
 
   s.resources    = "XLsn0w/Resources/XLsn0wKit.bundle"
 
@@ -23,7 +22,6 @@ Pod::Spec.new do |s|
 
   s.libraries = 'z', 'sqlite3'
   s.frameworks = 'UIKit', 'CoreFoundation', 'CoreText', 'CoreGraphics', 'CoreImage', 'QuartzCore', 'ImageIO', 'AssetsLibrary', 'Accelerate', 'MobileCoreServices', 'SystemConfiguration'
-  s.ios.vendored_frameworks = 'XLsn0w/Frameworks/WebP.framework'
 
   s.dependency "AFNetworking"
   s.dependency 'Masonry'
--- XLsn0wKit/ViewController.m
+++ XLsn0wKit/ViewController.m
@@ -1,23 +1,14 @@
 //
 //  ViewController.m
-//  WebImageDemo
+//  33
 //
-//  Created by YueWen on 16/3/20.
-//  Copyright © 2016年 YueWen. All rights reserved.
+//  Created by XLsn0w on 16/9/11.
+//  Copyright © 2016年 XLsn0w. All rights reserved.
 //
 
 #import "ViewController.h"
 
-#import "UIImageView+XLsn0wWebImage.h"
-#import "XLsn0wImageCache.h"
-
 @interface ViewController ()
-
-@property (weak, nonatomic) IBOutlet UIImageView *imageView;
-@property (weak, nonatomic) IBOutlet UILabel *label;
-@property (weak, nonatomic) IBOutlet UITextField *inputView;
-
-@property (weak, nonatomic) IBOutlet UILabel *lblFileSize;
 
 @end
 
@@ -25,61 +16,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    // Do any additional setup after loading the view, typically from a nib.
 }
-
-
-//开始加载图片
-- (IBAction)startLoadImage:(id)sender {
-    //默认初始化label为本地
-    self.label.text = @"本地图片！";
-
-    NSString *imageURL = @"http://news.hainan.net/Editor/img/201304/20130428/big/20130428114534772_9485643.jpg";
-    //可看进度的方法   
-    [self.imageView xlsn0w_setImageWithUrl:imageURL withProgressHandle:^(CGFloat didFinish, CGFloat didFinishTotal, CGFloat Total) {
-        
-        //更改Label
-        NSString * progress = [NSString stringWithFormat:@"%.1f%%",(didFinishTotal * 1.0 / Total) * 100.0];
-        self.label.text = progress;
-        
-    }];
-}
-
-
-//开始计算文件大小
-- (IBAction)lookFileSize:(id)sender
-{
-    NSString * fileSize = [NSString stringWithFormat:@"缓存大小为:%@MB", [[XLsn0wImageCache sharedImageCache] fileSize]];
-    
-    self.lblFileSize.text = fileSize;
-}
-
-
-//删除所有的文件
-- (IBAction)deleteAllFile:(id)sender {
-    [[XLsn0wImageCache sharedImageCache] deleteAllCAchesProgress:^(NSString *fileName) {
-        
-        //更改lable的显示
-        self.label.text = [NSString stringWithFormat:@"正在删除%@",fileName];
-        
-    } Complete:^{
-        
-        //更改lable的显示
-        self.label.text = @"删除完毕!";
-        self.imageView.image = [UIImage imageNamed:@""];
-        
-    }];
-}
-
-
-
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-
 
 @end
